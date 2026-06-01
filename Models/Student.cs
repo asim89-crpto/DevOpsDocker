6a1a5b6f060f2031a7ee74b58bb3832f6ffec6d7
Asim Awan <asimpakistan89@gmail.com>
2026-06-01 13:08:08 +0100

    Built Blazor CRUD app with SQL Server and Docker support

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentApp.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = "";
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = "";
+
+    [Required]
+    public string Course { get; set; } = "";
+}
